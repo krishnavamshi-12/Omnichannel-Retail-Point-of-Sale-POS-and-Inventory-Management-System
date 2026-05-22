fa05b7f23df420d82edf0cc7ac1da5e0f6ddb466
THUMMALAPALLI KRISHNA VAMSHI <kv9032425367@gmail.com>
2026-05-22 18:26:51 +0530

    Add files via upload

---
+++ generateToken.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (
+  userId: string,
+  role: string
+): string => {
+  return jwt.sign(
+    {
+      userId,
+      role
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "7d"
+    }
+  );
+};
+
+export default generateToken;
