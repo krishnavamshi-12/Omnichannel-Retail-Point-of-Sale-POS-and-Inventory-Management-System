8e05d5f50a99f6e78161c71a50f4fead183fb18e
THUMMALAPALLI KRISHNA VAMSHI <kv9032425367@gmail.com>
2026-05-23 19:06:29 +0530

    Add files via upload

--- redis.ts
+++ redis.ts
@@ -1,19 +1,26 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+
+  socket: {
+    reconnectStrategy: false,
+  },
 });
 
-redisClient.on("error", (err) => {
-  console.error("Redis Error:", err);
+// Redis Error Event
+redisClient.on("error", () => {
+  console.log("⚠️ Redis Server Not Running");
 });
 
+// Connect Function
 export const connectRedis = async (): Promise<void> => {
   try {
     await redisClient.connect();
-    console.log("Redis Connected Successfully");
+
+    console.log("✅ Redis Connected");
   } catch (error) {
-    console.error("Redis Connection Failed:", error);
+    // Optional: no extra log needed
   }
 };
 
